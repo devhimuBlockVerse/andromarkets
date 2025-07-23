b31161625f7f322e19d7c6f01685ff8db178fe95
Arefin Himu <himu.dev@blockversesolutions.com>
2025-07-23 16:18:34 +0600

    App Colors Setup

--- lib/config/theme/app_colors.dart
+++ lib/config/theme/app_colors.dart
@@ -1,11 +1,55 @@
 import 'dart:ui';
+import 'package:flutter/cupertino.dart';
 
 class AppColors {
-  static const Color primary = Color(0xFF0A84FF);
-  static const Color secondary = Color(0xFF5E5CE6);
-  static const Color background = Color(0xFFF2F2F7);
-  static const Color surface = Color(0xFFFFFFFF);
-  static const Color textPrimary = Color(0xFF1C1C1E);
-  static const Color textSecondary = Color(0xFF3A3A3C);
-  static const Color error = Color(0xFFFF3B30);
+  static const Color primaryColor = Color(0xFFFCD331);
+
+  static const Color primaryText = Color(0xFFFFFFFF);
+
+  static Color descriptions = Color(0xFFFFFFFF).withOpacity(0.8);
+
+  static const Color black = Color(0xFF191919);
+
+  static const Color redErrorCall = Color(0xFFFF3E70);
+
+  static const Color green = Color(0xFF58D58D);
+
+  static  Color secondaryColorDiscriptions2 = Color(0xFF58D58D).withOpacity(0.7);
+
+  static  Color secondaryColor3 = Color(0xFF58D58D).withOpacity(0.6);
+
+  static const Color gray = Color(0xFFE0E0E0);
+
+  static const LinearGradient goldGradient = LinearGradient(
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+    colors: [
+      Color(0xFFE99E10),
+      Color(0xFFFDD27E),
+      Color(0xFFE99E10),
+    ],
+    stops: [
+      0.3, // 30%
+      0.5, // 50%
+      0.7  // 70%
+    ],
+  );
+
+  static const LinearGradient yellowGradient = LinearGradient(
+    begin: Alignment.centerLeft,
+    end: Alignment.centerRight,
+    colors: [
+      Color(0xFFFFE500),
+      Color(0xFFDBC504),
+    ],
+    stops: [
+      0.0, // 0%
+      1.0, // 100%
+    ],
+  );
+
+  static const Color primaryButtonColor = Color(0xFFFCD331);
+
 }
+
+
